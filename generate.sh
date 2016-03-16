858f88b97e84e6cc922c1d73f8a6dfe6fd07d32c
Eyal Kalderon <ebkalderon@gmail.com>
2016-03-16 19:55:38 -0400

    Regenerate website

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 # Rebuilds the website, blog, book, and API documentation from scratch.
 
 echo "Cleaning up workspace..."
-rm -rf _posts doc blog blog.css book cover.css index.html
+rm -rf _posts doc blog.css book cover.css index.html
 
 git clone https://github.com/ebkalderon/amethyst
 cd amethyst
@@ -11,15 +11,15 @@ cargo doc --no-deps -p amethyst -p amethyst_engine
 mdbook build book
 cd ..
 
-echo "Building blog..."
-cobalt build -s blog
-
 echo "Copying files..."
 cp -r amethyst/book/html/ book
 cp -r amethyst/book/images/ book/images
 cp -r amethyst/target/doc/ .
 echo '<meta http-equiv="refresh" content="0; url=amethyst/" />' > doc/index.html
 
+echo "Building blog..."
+cobalt build -s blog/
+
 git add --all
-git commit -m "Regenerate website content"
+git commit -m "Regenerate website"
 git push -u origin master
