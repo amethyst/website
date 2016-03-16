7abb0375233a2237e8b163bdf4111e7057d228e9
Eyal Kalderon <ebkalderon@gmail.com>
2016-03-16 13:31:55 -0400

    Redirect with shorter URL in generate.sh

--- generate.sh
+++ generate.sh
@@ -13,7 +13,7 @@ cd ..
 cp -r amethyst/book/html/ book
 cp -r amethyst/book/images/ book/images
 cp -r amethyst/target/doc/ .
-echo '<meta http-equiv="refresh" content="0; url=amethyst/index.html" />' > doc/index.html
+echo '<meta http-equiv="refresh" content="0; url=amethyst/" />' > doc/index.html
 
 git add --all
 git commit -m "Regenerate website content"
