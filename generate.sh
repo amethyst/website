c65b3374936167fafe4ece8a1f797f7d8ccc36a2
Eyal Kalderon <ebkalderon@gmail.com>
2016-03-16 20:17:33 -0400

    Add .nojekyll

--- generate.sh
+++ generate.sh
@@ -5,13 +5,14 @@
 echo "Cleaning up workspace..."
 rm -rf _posts doc blog.css book cover.css index.html
 
+echo "Generating book and API docs..."
 git clone https://github.com/ebkalderon/amethyst
 cd amethyst
 cargo doc --no-deps -p amethyst -p amethyst_engine
 mdbook build book
 cd ..
 
-echo "Copying files..."
+echo "Copying files over..."
 cp -r amethyst/book/html/ book
 cp -r amethyst/book/images/ book/images
 cp -r amethyst/target/doc/ .
