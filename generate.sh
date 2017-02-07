be1216099f8ada4f22599bc81ba58fb49f7cb37b
Eyal Kalderon <ebkalderon@gmail.com>
2017-02-07 03:00:06 -0500

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -10,23 +10,18 @@ echo "Generating API docs..."
 echo "  Generating master branch docs"
 git clone https://github.com/amethyst/amethyst --branch master
 cd amethyst
-  cargo doc --no-deps -p amethyst -p amethyst_ecs -p amethyst_renderer -p amethyst_engine # TODO: need to update this on the next release
+  cargo doc --no-deps -p amethyst -p amethyst_config -p amethyst_renderer
 cd ..
 
 echo "  Generating develop branch docs"
 git clone -b develop https://github.com/amethyst/amethyst amethyst_dev
 cd amethyst_dev
-  cargo doc --no-deps -p amethyst -p amethyst_ecs -p amethyst_context -p amethyst_renderer
+  cargo doc --no-deps -p amethyst -p amethyst_config -p amethyst_renderer
 cd ..
 
-
-echo "Generating book..."
-git clone https://github.com/azerupi/mdBook
-cd mdBook
-git reset --hard 925939e26720e1998796a1735c296048c99ee7f8
-cargo build --release
-cd ..
-./mdBook/target/release/mdbook build amethyst/book
+echo "Compiling the book..."
+cargo install mdbook
+mdbook build amethyst/book
 
 echo "Copying files over..."
 cp -r amethyst/book/html/ build/book
@@ -41,4 +36,3 @@ echo "Building website from source..."
 cargo install cobalt-bin
 
 cobalt build
-
