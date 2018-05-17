18c99d3e011deed9464ef58ae473e44ab7627871
Jacob Kiesel <kieseljake@gmail.com>
2018-05-17 09:54:23 -0600

    Try a fix for book images

--- generate.sh
+++ generate.sh
@@ -49,8 +49,8 @@ do
     cp -r src/amethyst/${branches[$i]}/target/doc/ src/doc/${branches[$i]}/
 
     mkdir -p src/book/${branches[$i]}/
-    mv -f src/amethyst/${branches[$i]}/book/book/ src/book/${branches[$i]}/
-    mv -f src/amethyst/${branches[$i]}/book/images/ src/book/${branches[$i]}/
+    mv -f src/amethyst/${branches[$i]}/book/book/* src/book/${branches[$i]}/
+    cp -rf src/amethyst/${branches[$i]}/book/images/ src/book/${branches[$i]}/
 
     # Create branch documentation pages.
     docs_branch ${branches[$i]} src/doc/${branches[$i]}.html
